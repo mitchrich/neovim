3ee048457c8882ac60a8159618f9641a059a42f2
Mitchell Richardson <sitback0706@outlook.com>
2026-08-03 19:21:10 +1000

    Trimmed down plugins and updated lockfile

--- lua/plugins/add.lua
+++ lua/plugins/add.lua
@@ -6,8 +6,6 @@ vim.pack.add({
     'https://github.com/nvim-telescope/telescope.nvim',
     'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
     'https://github.com/nvim-lua/plenary.nvim', -- Telescope dependency
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/mason-org/mason-lspconfig.nvim",
     "https://github.com/lewis6991/gitsigns.nvim",
     "https://github.com/danymat/neogen",
     { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
